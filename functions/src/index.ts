2048a744c8f57fc2d7a09a77c507a160eb9f01e3
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-07-16 05:10:36 +0300

    Update package dependencies and refactor Firebase function to use v1 API. Simplified ESLint command and added bad-words package for profanity filtering in Firestore trigger.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,54 +7,30 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { setGlobalOptions } from "firebase-functions";
-import { onRequest } from "firebase-functions/https";
-import * as logger from "firebase-functions/logger";
-import { initializeApp } from "firebase/app";
+import * as functions from "firebase-functions/v1";
+import FilterImport from "bad-words";
+type BadWordsConstructor = new () => {
+  isProfane: (text: string) => boolean;
+  clean: (text: string) => string;
+};
+const Filter = FilterImport as unknown as BadWordsConstructor;
+import * as admin from "firebase-admin";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-const functions = require("firebase-functions");
-const Filter = require("bad-words");
-
-const admin = require("firebase-admin");
 admin.initializeApp();
 
 const db = admin.firestore();
 
-exports.detectEvilUsers = functions.firestore
+export const detectEvilUsers = functions.firestore
   .document("messages/{msgId}")
-  .onCreate(async (doc, ctx) => {
+  .onCreate(async (doc: functions.firestore.QueryDocumentSnapshot) => {
     const filter = new Filter();
     const { text, uid } = doc.data();
 
     if (filter.isProfane(text)) {
-
-        const cleaned = filter.clean(text);
-        await doc.ref.update({text: `🤐 I got banned for saying... ${cleaned}`});
-
-        await db.collection("banned").doc(uid).set({});
+      const cleaned = filter.clean(text);
+      await doc.ref.update({
+        text: `🤐 I got banned for saying... ${cleaned}`,
+      });
+      await db.collection("banned").doc(uid).set({});
     }
-
-
-
-
   });
