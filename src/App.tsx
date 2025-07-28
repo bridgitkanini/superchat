f150c34e085780b48309bd57aa748110953156e6
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-07-28 07:47:56 +0300

    Update firebase configuration

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,14 @@ import { Landing } from "./Landing";
 import { ChatRoomPage } from "./ChatRoomPage";
 import "./App.css";
 
-// Use environment variables if available, otherwise use the hardcoded values
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCv6QvaxzCw53Qu2egUPdMpAO2Nmb6MIpE",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "superchat-ea9dd.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "superchat-ea9dd",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "superchat-ea9dd.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "916381874487",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:916381874487:web:2a51f79b3cab96c25cc2b2",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-DGSN3K7ZH4",
+  apiKey: "AIzaSyCv6QvaxzCw53Qu2egUPdMpAO2Nmb6MIpE",
+  authDomain: "superchat-ea9dd.firebaseapp.com",
+  projectId: "superchat-ea9dd",
+  storageBucket: "superchat-ea9dd.firebasestorage.app",
+  messagingSenderId: "916381874487",
+  appId: "1:916381874487:web:2a51f79b3cab96c25cc2b2",
+  measurementId: "G-DGSN3K7ZH4",
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
